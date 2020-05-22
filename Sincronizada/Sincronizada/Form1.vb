2a8269c14119612faf5cc3e5bae2646cc0582350
miriaampriscilaa@hotmail.com <miriaampriscilaa@hotmail.com>
2020-05-21 20:51:49 -0500

    SUERTE CON TU SINCRONIZADA

--- Sincronizada/Sincronizada/Form1.vb
+++ Sincronizada/Sincronizada/Form1.vb
@@ -4,6 +4,8 @@
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         Dim Video As New Tortilla
         Video.Receta()
-        MessageBox.Show("SUERTE CON TU SINCRONIZADA")
+        If MessageBox.Show("SUERTE CON TU SINCRONIZADA") = DialogResult.OK Then
+            Application.Exit()
+        End If
     End Sub
 End Class

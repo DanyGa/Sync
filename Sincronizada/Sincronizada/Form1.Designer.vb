ae41aba67ae265a377287d03d38c2b64d4967cb6
miriaampriscilaa@hotmail.com <miriaampriscilaa@hotmail.com>
2020-05-21 20:10:30 -0500

    NUEVA IMAGEN

--- Sincronizada/Sincronizada/Form1.Designer.vb
+++ Sincronizada/Sincronizada/Form1.Designer.vb
@@ -24,16 +24,16 @@ Partial Class FrmQuesadilla
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.lblTortila = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.Sincronizada.My.Resources.Resources.sync
-        Me.PictureBox1.Location = New System.Drawing.Point(16, 59)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Location = New System.Drawing.Point(54, 137)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(399, 271)
+        Me.PictureBox1.Size = New System.Drawing.Size(299, 220)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
@@ -41,20 +41,34 @@ Partial Class FrmQuesadilla
         'lblTortila
         '
         Me.lblTortila.AutoSize = True
-        Me.lblTortila.Location = New System.Drawing.Point(171, 38)
+        Me.lblTortila.Font = New System.Drawing.Font("Century Gothic", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTortila.Location = New System.Drawing.Point(133, 18)
+        Me.lblTortila.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTortila.Name = "lblTortila"
-        Me.lblTortila.Size = New System.Drawing.Size(53, 17)
+        Me.lblTortila.Size = New System.Drawing.Size(120, 38)
         Me.lblTortila.TabIndex = 1
         Me.lblTortila.Text = "Torti-la"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(370, 48)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "PRESIONA LA IMAGEN PARA SABER " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "CÓMO HACER UNA SINCRONIZADA"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'FrmQuesadilla
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(889, 454)
+        Me.ClientSize = New System.Drawing.Size(396, 369)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblTortila)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FrmQuesadilla"
         Me.Text = "FrmQuesadilla"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -65,4 +79,5 @@ Partial Class FrmQuesadilla
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents lblTortila As Label
+    Friend WithEvents Label1 As Label
 End Class

c7671325ff78f35b111426d6b6b04dfa5b7923ab
Daniela García <DanyGa02@hotmail.com>
2020-05-21 19:45:28 -0500

    Tor-la Label

--- Sincronizada/Sincronizada/Form1.Designer.vb
+++ Sincronizada/Sincronizada/Form1.Designer.vb
@@ -23,7 +23,7 @@ Partial Class FrmQuesadilla
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.rdbTotilla = New System.Windows.Forms.RadioButton()
+        Me.lblTortila = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -31,30 +31,28 @@ Partial Class FrmQuesadilla
         '
         Me.PictureBox1.Image = Global.Sincronizada.My.Resources.Resources.sync
         Me.PictureBox1.Location = New System.Drawing.Point(16, 59)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(399, 271)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'rdbTotilla
+        'lblTortila
         '
-        Me.rdbTotilla.AutoSize = True
-        Me.rdbTotilla.Location = New System.Drawing.Point(168, 31)
-        Me.rdbTotilla.Name = "rdbTotilla"
-        Me.rdbTotilla.Size = New System.Drawing.Size(72, 21)
-        Me.rdbTotilla.TabIndex = 1
-        Me.rdbTotilla.TabStop = True
-        Me.rdbTotilla.Text = "Tortilla"
-        Me.rdbTotilla.UseVisualStyleBackColor = True
+        Me.lblTortila.AutoSize = True
+        Me.lblTortila.Location = New System.Drawing.Point(171, 38)
+        Me.lblTortila.Name = "lblTortila"
+        Me.lblTortila.Size = New System.Drawing.Size(53, 17)
+        Me.lblTortila.TabIndex = 1
+        Me.lblTortila.Text = "Torti-la"
         '
         'FrmQuesadilla
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(889, 454)
-        Me.Controls.Add(Me.rdbTotilla)
+        Me.Controls.Add(Me.lblTortila)
         Me.Controls.Add(Me.PictureBox1)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "FrmQuesadilla"
@@ -66,5 +64,5 @@ Partial Class FrmQuesadilla
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents rdbTotilla As RadioButton
+    Friend WithEvents lblTortila As Label
 End Class
--- Sincronizada/Sincronizada/Form1.vb
+++ Sincronizada/Sincronizada/Form1.vb
@@ -5,15 +5,4 @@
         Dim Video As New Tortilla
         Video.Receta()
     End Sub
-
-    Private Sub RbnTotilla_CheckedChanged(sender As Object, e As EventArgs) Handles rdbTotilla.CheckedChanged
-        'A PARTIR DE AQUI ES INUTIL SE VA  A LA PAGINA SI LE PICAS A LA IMAGEN
-        Select Case Helo
-            Case 1
-                If (Me.rdbTotilla.Checked = True) Then
-
-                End If
-        End Select
-        'es algo asi lol solo que si le picas no te lleva a la pagina but idk 
-    End Sub
 End Class
